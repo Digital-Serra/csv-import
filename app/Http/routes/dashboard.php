48ef0c45cdae50188a9991c56e0d5c88482851e5
Mauri de Souza Nunes <mauri870@gmail.com>
2015-10-26 13:54:18 -0200

    Removed duplicate route for dashboard.index

--- app/Http/routes/dashboard.php
+++ app/Http/routes/dashboard.php
@@ -2,7 +2,6 @@
 //DashBoard Routes
 Route::group(['middleware' => 'auth','namespace'=>'Dashboard'], function () {
     Route::get('/dashboard',['as'=>'dashboard.index','uses'=>'DashController@index']);
-    Route::get('/dashboard',['as'=>'dashboard.index','uses'=>'DashController@index']);
 
     // Import
     Route::get('/dashboard/import',['as'=>'dashboard.getImport','uses'=>'NewsController@getImport']);
