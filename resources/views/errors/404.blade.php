bb03e3aa29b6faa3daff920117cf3556db8a3d74
Mauri de Souza Nunes <mauri870@gmail.com>
2015-10-26 13:46:25 -0200

    Added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Missing Page - 404</title>
+    <style type="text/css">
+        body { color: #fff; text-shadow: 0 1px 2px #000; font-family: "chaparral-pro", 'Helvetica Neue', arial, sans-serif; }
+        div.dialog {
+            width: 40%;
+            padding: 2em;
+            margin: 2em auto 0 2em;
+        }
+        h1 {
+            font-style:italic;
+            font-size:40px;
+            line-height:48px;
+            margin-bottom:.5em;
+        }
+        p {
+            font-size:20px;
+            line-height:30px;
+        }
+        a {
+            color:#fff;
+            font-weight:bold;
+            text-decoration:none;
+            border-bottom: 2px solid rgba(255,255,255,.5);
+        }
+        a:hover {
+            border-bottom: 2px solid rgba(255,255,255,1);
+        }
+    </style>
+    <meta charset="utf-8">
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="http://s.cdpn.io/13060/jquery.anystretch.min_1.js"></script>
+</head>
+
+<body>
+<!-- This file lives in public/404.html -->
+<div class="dialog">
+    <h1>Yeah, that shit's gone.</h1>
+    <p>We've sent a pledge to find what you're looking for, but who knows how long that'll take. You're better off going back to the <a href="http://fratfolder.com" alt="Fratfolder.com">home page</a> or letting our <a href="mailto:admin@fratfolder.com" alt="E-mail Us">academic chair</a> know about this.</p>
+    <p>This is a great opportunity to take a quick study break with a beer and reflect on how much ass you're going to kick on that test tomorrow.</p>
+</div>
+
+<script type="text/javascript">
+    //http://i.imgur.com/3mykX.gif
+    $.anystretch("http://payload202.cargocollective.com/1/13/430252/6345864/Gif-Vache-_600.gif", {speed: 150});
+</script>
+
+</body>
+</html>
