ff665aeed4b44fe256a3d6924f82dfc3c0085ed8
Mauri de Souza Nunes <mauri870@gmail.com>
2015-10-26 09:58:47 -0200

    Created footer for emails

--- resources/views/emails/includes/footer.blade.php
+++ resources/views/emails/includes/footer.blade.php
@@ -1,7 +1,32 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: mauri870
- * Date: 26/10/15
- * Time: 08:32
- */
+<tr>
+    <td align="center" valign="top">
+        <!-- CENTERING TABLE // -->
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <tr>
+                <td align="center" valign="top">
+                    <!-- FLEXIBLE CONTAINER // -->
+                    <table border="0" cellpadding="0" cellspacing="0" width="500" class="flexibleContainer">
+                        <tr>
+                            <td align="center" valign="top" width="500" class="flexibleContainerCell">
+                                <table border="0" cellpadding="30" cellspacing="0" width="100%">
+                                    <tr>
+                                        <td valign="top" bgcolor="#E1E1E1">
+
+                                            <div style="font-family:Helvetica,Arial,sans-serif;font-size:13px;color:#828282;text-align:center;line-height:120%;">
+                                                <div>Copyright &#169; {{ date('Y') }} <a href="http://digitalserra.com.br" target="_blank" style="text-decoration:none;color:#828282;"><span style="color:#828282;">Digital&nbsp;Serra</span></a>. Todos&nbsp;os&nbsp;direitos&nbsp;reservados</div>
+                                                <div>Se você não deseja receber e-mails nossos, você pode cancelar <a href="{{ url() }}/unsubscribe/{{ $token }}" target="_blank" style="color:#000000;"><span style="color:#000000;">aqui</span></a>.</div>
+                                            </div>
+
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                    <!-- // FLEXIBLE CONTAINER -->
+                </td>
+            </tr>
+        </table>
+        <!-- // CENTERING TABLE -->
+    </td>
+</tr>
