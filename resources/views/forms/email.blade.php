5205a64bde7b0fee45c9e41e0b6115e0cac81c7c
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-04 13:09:49 -0200

    Created form for emails

--- resources/views/forms/email.blade.php
+++ resources/views/forms/email.blade.php
@@ -1,3 +1,9 @@
-<div class="form-input">
-    {!! Form::file('table') !!}
+<div class="form-group">
+    {!! Form::label('name','Nome') !!}
+    {!! Form::text('name',null,['class'=>'form-control']) !!}
+
+</div>
+<div class="form-group">
+    {!! Form::label('email','Email') !!}
+    {!! Form::text('email',null,['class'=>'form-control']) !!}
 </div>
