8ffdb89a8852134063240fbe65f3e00d3bb81d4e
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-04 08:51:04 -0200

    Use the mailgun facade inside controller

--- app/Http/Controllers/Dashboard/ImportController.php
+++ app/Http/Controllers/Dashboard/ImportController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Email;
-use App\Jobs\SendEmail;
-use Bogardo\Mailgun\Mailgun;
+use Bogardo\Mailgun\Facades\Mailgun;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Requests;
